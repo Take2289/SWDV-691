342b3f0ade2c30066c9b90f2f5b37bb6ff1ef290
Ty Davidson <62067132+Take2289@users.noreply.github.com>
2021-07-28 16:37:02 -0400

    github

--- four-leaf/src/App.vue
+++ four-leaf/src/App.vue
@@ -1,9 +1,10 @@
+
 <template>
   <div id="app">
     <div id="nav">
+      <router-link to="/Login">Login</router-link> |
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
-      <router-link to="/Login">Login</router-link> |
       <router-link to="/Inventory">Inventory</router-link>
     </div>
     <router-view/>
--- four-leaf/src/plugins/vuetify.js
+++ four-leaf/src/plugins/vuetify.js
@@ -1,7 +1,20 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib/framework';
-
+import colors from 'vuetify/lib/util/colors';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    icons: {
+        iconfont: 'md' || 'fa' 
+      },
+      theme: {
+        themes: {
+            light: {
+                background: colors.grey.lighten2, 
+              },
+          dark: {
+            background: colors.shades.white, 
+          },
+        },
+      },
 });
